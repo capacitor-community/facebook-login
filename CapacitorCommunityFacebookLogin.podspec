3a64a5f28a1a7adf1ba807ee74fff8cffd4d4065
榊原昌彦 <sakakibara@rdlabo.jp>
2020-06-26 15:52:51 +0900

    init(initial): initial each package

---
+++ CapacitorCommunityFacebookLogin.podspec
@@ -0,0 +1,14 @@
+  Pod::Spec.new do |s|
+    s.name = 'CapacitorFacebookLogin'
+    s.version = '1.1.0'
+    s.summary = 'Facebook Login plugin for Capacitor'
+    s.license = 'MIT'
+    s.homepage = 'https://github.com/capacitor-community/facebook-login'
+    s.author = 'Masahiko Sakakibara'
+    s.source = { :git => 'https://github.com/capacitor-community/facebook-login', :tag => s.version.to_s }
+    s.source_files = 'ios/Plugin/Plugin/**/*.{swift,h,m,c,cc,mm,cpp}'
+    s.ios.deployment_target  = '11.0'
+    s.dependency 'Capacitor'
+    s.dependency 'FacebookCore', "0.9.0"
+    s.dependency 'FacebookLogin', "0.9.0"
+  end
---
+++ ios/Plugin/Plugin.swift
@@ -0,0 +1,81 @@
+import Foundation
+import Capacitor
+import FacebookCore
+import FacebookLogin
+
+/**
+ * Please read the Capacitor iOS Plugin Development Guide
+ * here: https://capacitor.ionicframework.com/docs/plugins/ios
+ */
+@objc(FacebookLogin)
+public class FacebookLogin: CAPPlugin {
+    private let loginManager = LoginManager()
+    private let dateFormatter = ISO8601DateFormatter()
+
+    override public func load() {
+        if #available(iOS 11, *) {
+            dateFormatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+        } else {
+            dateFormatter.formatOptions = [.withInternetDateTime]
+        }
+
+    }
+
+    private func dateToJS(_ date: Date) -> String {
+        return dateFormatter.string(from: date)
+    }
+
+    @objc func login(_ call: CAPPluginCall) {
+        guard let permissions = call.getArray("permissions", String.self) else {
+            call.error("Missing permissions argument")
+            return;
+        }
+
+        let perm = permissions.map { Permission.custom($0) }
+
+        DispatchQueue.main.async {
+            self.loginManager.logIn(permissions: perm, viewController: self.bridge.viewController) { loginResult in
+                switch loginResult {
+                case .failed(let error):
+                    print(error)
+                    call.reject("LoginManager.logIn failed")
+
+                case .cancelled:
+                    print("User cancelled login")
+                    call.success()
+
+                case .success(let grantedPermissions, let declinedPermissions, let accessToken):
+                    print("Logged in")
+                    return self.getCurrentAccessToken(call)
+                }
+            }
+        }
+    }
+
+    @objc func logout(_ call: CAPPluginCall) {
+        loginManager.logOut()
+
+        call.success()
+    }
+
+    private func accessTokenToJson(_ accessToken: AccessToken) -> [String: Any?] {
+        return [
+            "applicationId": accessToken.appID,
+            /*declinedPermissions: accessToken.declinedPermissions,*/
+            "expires": dateToJS(accessToken.expirationDate),
+            "lastRefresh": dateToJS(accessToken.refreshDate),
+            /*permissions: accessToken.grantedPermissions,*/
+            "token": accessToken.tokenString,
+            "userId": accessToken.userID
+        ]
+    }
+
+    @objc func getCurrentAccessToken(_ call: CAPPluginCall) {
+        guard let accessToken = AccessToken.current else {
+            call.success()
+            return
+        }
+
+        call.success([ "accessToken": accessTokenToJson(accessToken) ])
+    }
+}
