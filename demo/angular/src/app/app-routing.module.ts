85b19eb296837333347bb0abdd09e3d576a2e4e9
榊原昌彦 <sakakibara@rdlabo.jp>
2023-05-05 14:15:55 +0900

    feat(demo): update to ng15

--- demo/angular/src/app/app-routing.module.ts
+++ demo/angular/src/app/app-routing.module.ts
@@ -18,9 +18,8 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      preloadingStrategy: PreloadAllModules,
-      relativeLinkResolution: 'legacy',
-    }),
+    preloadingStrategy: PreloadAllModules
+}),
   ],
   exports: [RouterModule],
 })
