a0930a1d572b6b120a306b40f7f1de74ad56e7c3
榊原昌彦 <sakakibara@rdlabo.jp>
2023-09-06 17:15:43 +0900

    chore(demo): ng update to 16

--- demo/angular/src/app/auth/auth.guard.ts
+++ demo/angular/src/app/auth/auth.guard.ts
@@ -1,16 +1,11 @@
 import { Injectable } from '@angular/core';
-import {
-  CanActivate,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-  Router,
-} from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(private auth: AuthService, private router: Router) {}
 
   async canActivate(
