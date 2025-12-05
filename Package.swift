4a0c2e3261649963330d1a5227c6823f8ddc6ae3
jcesarmobile <jcesarmobile@gmail.com>
2025-12-05 17:47:05 +0100

    feat(ios): update facebook SDK

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0"),
-        .package(url: "https://github.com/facebook/facebook-ios-sdk.git", from: "17.4.0")
+        .package(url: "https://github.com/facebook/facebook-ios-sdk.git", from: "18.0.2")
     ],
     targets: [
         .target(
