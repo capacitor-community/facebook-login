c1b50899ccbb6398eca2699d5c61bc90e7b58301
rdlabo <sakakibara@rdlabo.jp>
2025-12-20 22:03:03 +0900

    chore(demo): update android files

--- demo/angular/android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ demo/angular/android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,15 +1,5 @@
 package io.ionic.starter;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Plugin;
-import java.util.ArrayList;
 
-public class MainActivity extends BridgeActivity {
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        registerPlugin(com.getcapacitor.community.facebooklogin.FacebookLogin.class);
-    }
-}
+public class MainActivity extends BridgeActivity {}
