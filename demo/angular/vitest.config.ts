2779bcee2156522bb64ac4cf386cac754716d6c0
rdlabo <sakakibara@rdlabo.jp>
2026-08-14 16:24:05 +0900

    modernize Angular demo tooling

--- demo/angular/vitest.config.ts
+++ demo/angular/vitest.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    setupFiles: ['src/test-setup.ts'],
     server: {
       deps: {
         inline: [/@ionic\/angular/, /@ionic\/core/, /ionicons/],
